b35bb75950096105e9080342b20f40dcbb3e8698
b0da1eb136b0bbcb691a44eba2704211b841bdf1 <adersh.rp@in.ibm.com>
2020-09-21 00:03:40 +0530

    update on switch

--- switchexample.go
+++ switchexample.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	number := 8
+	//switch with multiple checks in one case
 	switch number {
 	case 1, 3, 5, 7, 9:
 		fmt.Println("Odd Number")
@@ -25,10 +26,21 @@ func main() {
 		fmt.Println("Weekday")
 
 	}
+
+	// switch with initializer
 	switch i := 1 + 4; i {
 	case 1, 3, 5, 7, 9:
 		fmt.Println("Odd Number", i)
 	case 2, 4, 6, 8, 10:
 		fmt.Println("Even Number", i)
+	}
+
+	// variance of switch without expression, case level expression
+	numberValue := 10454
+	switch {
+	case numberValue%2 != 0:
+		fmt.Println("Odd Number", numberValue)
+	case numberValue%2 == 0:
+		fmt.Println("Even Number", numberValue)
 	}
 }
