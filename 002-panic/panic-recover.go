0f0a46f6ea547106652a47018d709c76a56c90f8
adershrp <adersh.rp.85@gmail.com>
2020-09-21 14:04:03 +0530

    update

---
+++ 002-panic/panic-recover.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Start")
+	panicker()
+	log.Println("End")
+}
+func panicker() {
+	log.Println("About to Panic")
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error", err) // eating the error. hence line10 end will print
+			panic(err)                // rethrowing the error. this wil not let line10 to execute
+		}
+	}()
+	panic("Something Happened")
+	log.Println("Done Panicing")
+}
