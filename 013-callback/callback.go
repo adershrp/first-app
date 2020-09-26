d0c8b2e1084f0510201ffee67852842ff03ce932
adershrp <adersh.rp.85@gmail.com>
2020-09-26 22:25:58 +0530

    update

---
+++ 013-callback/callback.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//* defining call back func as anonymous
+	cb1 := func(x int) string {
+		return fmt.Sprint("Result :\t", x)
+	}
+
+	//* defining call back func as anonymous
+	cb2 := func(x int) string {
+		return fmt.Sprint("Answer :\t", x)
+	}
+
+	//same add has two callback
+	fmt.Println(add(10, 20, cb1))
+	fmt.Println(add(10, 20, cb2))
+
+	fmt.Println(sub(10, 20, cb2))
+	fmt.Println(mul(10, 20, cb1))
+	fmt.Println(div(10, 20, cb2))
+
+}
+
+//add with callback
+func add(x, y int, c func(x int) string) string {
+	return c(x + y)
+}
+
+//sub with callback
+func sub(x, y int, c func(x int) string) string {
+	return c(x - y)
+}
+
+//mul with callback
+func mul(x, y int, c func(x int) string) string {
+	return c(x * y)
+}
+
+//div with callback
+func div(x, y int, c func(x int) string) string {
+	return c(x / y)
+}
---
+++ 014-closure/incrementer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := incrementer()
+	b := incrementer()
+
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(a())
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
+}
+
+func incrementer() func() int {
+	// !scope of x is between this block
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
